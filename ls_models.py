be15498440e011b3490c29a5a83ca7f243774208
Alex Bezzubov <bzz@apache.org>
2021-10-08 11:17:20 +0200

    lm_models: fix bug in checkpoint order + work \w local fs

--- ls_models.py
+++ ls_models.py
@@ -13,15 +13,19 @@ def sizeof_fmt(num):
 print("\t{:35} {:6} {:6}/{} {}".format("Model", "tokens", "steps", "ch", "tokens_per_batch"))
 print("\t----------------------")
 
-MODELS_DIR="gs://t5-codex/models"
+MODELS_DIR="gs://t5-codex/models" # or "./models"
 for dir in tf.io.gfile.glob(f"{MODELS_DIR}/*"):
+    if not os.path.isdir(dir):
+        continue
     model = os.path.basename(dir)
+    if model.endswith("-top5k"):
+        continue
     checkpoints = tf.io.gfile.glob(f"{MODELS_DIR}/{model}/model.ckpt-*.meta")
-    checkpoints = [ os.path.basename(ch).split("-")[-1].removesuffix(".meta") for ch in checkpoints ]
+    checkpoints = [ int(os.path.basename(ch).split("-")[-1].removesuffix(".meta")) for ch in checkpoints ]
     checkpoints = sorted(checkpoints)
 
     num_checkpoints=len(checkpoints)
-    steps=int(checkpoints[-1])
+    steps=int(checkpoints[-1]) if num_checkpoints > 0 else 0
 
     batch_size=None
     with tf.io.gfile.GFile(f"{MODELS_DIR}/{model}/operative_config.gin") as f:
