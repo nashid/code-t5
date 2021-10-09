fe2e57f2915fcb2ccb629891650faf8e8c051cb3
Alex Bezzubov <bzz@apache.org>
2021-10-09 13:36:26 +0200

    add missing examples for local inference

---
+++ mock_data.py
@@ -0,0 +1,33 @@
+bubble_sort = """from typing import List
+
+def bubble_sort(numbers: List[int]):
+    \"\"\" Sort given array of numbers in assending order using Bubble Sort algorithm.
+    >>> bubble_sort([3.0, 2.0, 1.0])
+    [1.0, 2.0, 3.0]
+
+    >>> bubble_sort([3.0, 1.0, 2.0])
+    [1.0, 2.0, 3.0]
+    \"\"\"
+""".replace("\n", "Ċ")
+
+has_close = """from typing import List
+
+def has_close_elements(numbers: List[float], threshold: float) -> bool:
+    \"\"\" Check if in given list of numbers, are any two numbers closer to each other than
+    given threshold.
+    >>> has_close_elements([1.0, 2.0, 3.0], 0.5)
+    False
+    >>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)
+    True
+    \"\"\"
+""".replace("\n", "Ċ")
+
+# Generate some predictions
+inputs = [
+    "public static void ",
+    "import ten ",
+    "Copyright ",
+    "open a file 'f.txt' in write mode ",
+    bubble_sort,
+    has_close,
+]
