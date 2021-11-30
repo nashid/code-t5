84012191242f707979a76db6ced78ac42ab71656
Alex Bezzubov <bzz@apache.org>
2021-11-30 19:52:36 +0100

    preprocessors: example of line-based input

--- codeT5/data/preprocessors.py
+++ codeT5/data/preprocessors.py
@@ -1,6 +1,7 @@
 
 import tensorflow as tf
 
+NEWLINE = 'Ċ'
 
 def fl_preprocessor(ds):
     def _to_inputs_and_targets(ex):
@@ -11,3 +12,16 @@ def fl_preprocessor(ds):
     return ds.map(_to_inputs_and_targets,
                 num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
+def split_lines(ds):
+    """Split each text documemnt (file) by line, keeping the newlines.
+       Should be applied before seqio.preprocessors.tokenize.
+    """
+    def _append_newline(x):
+        x = tf.expand_dims(x, axis=-1)
+        x = tf.concat([x, tf.broadcast_to([NEWLINE], x.shape)], axis=-1)
+        return tf.strings.reduce_join(x, axis=-1)
+
+    return ds\
+        .map(lambda x: tf.strings.split(x, NEWLINE))\
+        .flat_map(lambda x: tf.data.Dataset.from_tensor_slices(x))\
+        .map(_append_newline)
---
+++ unit_tests.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Runs unit-tests
+python3 codeT5/data/preprocessors_test.py
